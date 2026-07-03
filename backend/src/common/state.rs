62b318843dbf9ef16aba2eb25721f35889077d7e
M. Fritz <mathisjulian123@gmail.com>
2026-07-03 21:58:29 +0200

    feat: add valkey connection fr session storage

--- backend/src/common/state.rs
+++ backend/src/common/state.rs
@@ -1,6 +1,8 @@
 use std::{collections::HashMap, sync::Arc};
 
 use chrono::{DateTime, Duration, Utc};
+use deadpool_redis::{Config, Runtime};
+use redis::AsyncCommands;
 use sqlx::{PgPool, postgres::PgPoolOptions};
 use tokio::sync::RwLock;
 use tower_cookies::Key;
@@ -26,6 +28,7 @@ impl Session {
 pub struct AppState {
     pub pool: PgPool,
     pub cookie_key: Arc<Key>,
+	pub redis: deadpool_redis::Pool,
 	pub sessions: Arc<RwLock<HashMap<String, Session>>>
 }
 
@@ -36,6 +39,9 @@ impl AppState {
 
 		let db_url = std::env::var("DATABASE_URL")
 			.expect("DATABASE_URL must be set");
+
+		let redis_url = std::env::var("REDIS_URL")
+			.expect("REDIS_URL must be set");
 		
 		let pool = match PgPoolOptions::new()
 			.max_connections(10)
@@ -52,10 +58,29 @@ impl AppState {
 			}
 		};
 
+		let redis = match Config::from_url(redis_url).create_pool(Some(Runtime::Tokio1)) {
+			Ok(pool) => pool,
+			Err(err) => {
+				eprintln!("Invalid Redis config: {}", err);
+				std::process::exit(1);
+			}
+		};
+		
+		// Test connection to redis, because create_pool is lazy
+		match redis.get().await {
+			Ok(_) => {
+				println!("Connected to Redis successfully");
+			}
+			Err(err) => {
+				eprintln!("Failed to connect to Redis: {}", err);
+				std::process::exit(1);
+			}
+		};
 
 		Self {
 			pool: pool.clone(),
 			cookie_key: Arc::new(Key::from(secret.as_bytes())),
+			redis: redis.clone(),
 			sessions: Arc::new(RwLock::new(HashMap::new())),
 		}
 	}
